b6b9d6bcfc7616d96fb978f6e6f4c727374f58f4
Paweł Graczyk <pawgra7@gmail.com>
2022-07-02 19:19:49 +0200

    Initial commit

---
+++ src/main/java/com/example/kochbuch/KochBuchApplication.java
@@ -0,0 +1,13 @@
+package com.example.kochbuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KochBuchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KochBuchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kochbuch/KochBuchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kochbuch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KochBuchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
